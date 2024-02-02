b75319e66e8a30092dc92b55fbbf202f23b57d7a
Tibo <t.mousset@gmail.com>
2024-02-02 14:53:13 +0100

    Added slideshow in sopra steria

--- my_assets/js/script.js
+++ my_assets/js/script.js
@@ -195,3 +195,42 @@ if (!is_page_found) {
   navigationLinks[0].classList.add("active");
   window.scrollTo(0, 0);
 }
+
+
+
+
+// FOR IMAGE SLIDES
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+// make it slides automatically
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 5000); // Change image every 5 seconds
+// }
