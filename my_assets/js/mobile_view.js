08add81d6863a7eb26cdd063d56a3a08522510f4
Tibo <t.mousset@gmail.com>
2024-11-25 22:59:56 +0100

    mobile only page

---
+++ my_assets/js/mobile_view.js
@@ -0,0 +1,7 @@
+if (window.innerWidth <= 768) {
+    document.body.innerHTML = `
+    <p id="mobile_only_text">
+        This page is only available on desktop. <br> <br>
+        (Sorry 😭)
+    </p>`;
+}
