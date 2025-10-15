a7ed6987f5e7bedd8bfae4f7d08392b89adc5d19
Tibo <t.mousset@gmail.com>
2025-10-15 23:19:17 +0200

    Website available on mobile now yipee

--- my_assets/js/mobile_view.js
+++
@@ -1,7 +0,0 @@
-if (window.innerWidth <= 768) {
-    document.body.innerHTML = `
-    <p id="mobile_only_text">
-        This page is only available on desktop. <br> <br>
-        (Sorry 😭)
-    </p>`;
-}
